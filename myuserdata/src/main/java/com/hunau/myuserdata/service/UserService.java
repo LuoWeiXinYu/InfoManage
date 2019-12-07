5074a1906634bf4d3070fe24697ed4f855986b88
LuoWeiXinYu <328541570@qq.com>
2019-12-07 17:42:12 +0800

    用户登录

---
+++ myuserdata/src/main/java/com/hunau/myuserdata/service/UserService.java
@@ -0,0 +1,12 @@
+package com.hunau.myuserdata.service;/* *
+ * @Description:
+ * @param $params$
+ * @Return: $returns$
+ * @开发人员：余新伟
+ * @开发单位：湖南农业大学物联网工程专业
+ * @Date: 2019/10/18 15:49
+ * @开发版本：综合练习V0.1
+ */
+
+public class UserService {
+}
